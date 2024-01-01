d56b8bb502a804ac3f872a23e98b89c7237294ef
Kaveri Chougule <chougulekaveri123@gmail.com>
2024-01-01 13:13:28 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import React from 'react';
 import Progress from './Components/Progress';
 function App() {
+  document.title="Progress Bar"
   return (
     <div className="App">
       <Progress />
